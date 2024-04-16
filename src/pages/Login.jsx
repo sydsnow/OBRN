338bb2e560f23e54a5ed9e4d532b827c8e847396
Pamela Pragides <ppragides1@my.bcit.ca>
2024-04-16 13:06:21 -0700

    login page done + mobile

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,33 @@
-import loginimg from '../assets/login-img.jpeg';
+import loginimg from '../assets/login-img.jpg';
 
 function Login() {
   return (
     <div className="wrapper">
 <div className="login">
     <div className="login-container">
+
+        <form className="login-form">
+        <div className="login-header">
         <h1>Login</h1>
         <p>Access back to your account</p>
-        <form>
+        </div>
         <div className="form-group">
-            <label htmlFor="email">            <i className="fa-regular fa-envelope"></i>      Email</label>
-            <input type="email" id="email" name="email" />
+        <input className='input' type="email" required autoComplete='off' id="email" name="email" />
+
+        <label className="label" htmlFor="email">            <i className="fa-regular fa-envelope"></i> Email</label>
 
         </div>
         <div className="form-group">
-            <label htmlFor="password"> <i className="fa-solid fa-key"></i> Password</label>
-            <input type="password" id="password" name="password" />
+        <input className='input' type="password" id="password" required autoComplete='off' name="password" />
+            <label className="label" htmlFor="password"> <i className="fa-solid fa-key"></i> Password</label>
+
         </div>
         <button type="submit">Login</button>
         </form>
     </div>
     <div className="login-aside">
-        <div className="login-img">
+
     <img src={loginimg} alt="person loggin in" />
-    </div>
     </div>
 </div>
 </div>
