1948c0afd1d3d5f1d9a7e2b6824c7d2b9c06f635
Sydnee Snowball <s.snowball@hotmail.com>
2024-04-17 11:30:45 -0700

    more testimonials styling

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -1,4 +1,5 @@
 import "../scss/components/_testimonials.scss"; 
+import kitty from "../assets/kitty.jpg";
 
 function TestimonialsPage() {
 
@@ -8,19 +9,21 @@ function TestimonialsPage() {
       name: "John Doe",
       description: "Lorem ipsum dolor sit amet, ",
       rating: 5,
-      //image: 
+      image: kitty 
     },
     {
       id: 2,
       name: "Jane Doe",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in quam in odio venenatis luctus. Nullam at luctus enim. Nullam in quam in odio venenatis luctus. Nullam at luctus enim. Nullam in quam in odio venenatis luctus. Nullam at luctus enim.",
       rating: 4,
+      image: kitty
     },
     {
       id: 3,
       name: "James Doe",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in quam in odio venenatis luctus. Nullam at luctus enim. Nullam in quam in odio venenatis luctus. Nullam at luctus enim. Nullam in quam in odio venenatis luctus. Nullam at luctus enim.",
       rating: 3,
+      image: kitty 
     }
   ];
 
@@ -39,16 +42,21 @@ function TestimonialsPage() {
       <div className="testimonials-gallery">
         {testimonials.map((testimonial) => (
           <div className="testimonials-card" key={testimonial.id}>
-            <p className="testimonials-card-rating">
-                {/* need to make sure these work with multiple ratings and not just in 20% increments */}
-              {[...Array(5)].map((_, index) => (
-                <i key={index} className={`fas fa-star ${index < testimonial.rating ? 'filled' : ''}`}></i>
-              ))}
-            </p>
-            <p className="testimonials-card-description">
-              {testimonial.description.length > 100 ? `${testimonial.description.slice(0, 200)}...` : testimonial.description}
-            </p>
-            <p className="testimonials-card-name">{testimonial.name}</p>
+            <div className="testimonials-card-info">
+                <p className="testimonials-card-info-rating">
+                    {/* need to make sure these work with multiple ratings and not just in 20% increments */}
+                {[...Array(5)].map((_, index) => (
+                    <i key={index} className={`fas fa-star ${index < testimonial.rating ? 'filled' : ''}`}></i>
+                ))}
+                </p>
+                <p className="testimonials-card-info-description">
+                {testimonial.description.length > 100 ? `${testimonial.description.slice(0, 200)}...` : testimonial.description}
+                </p>
+                <p className="testimonials-card-info-name">{testimonial.name}</p>
+            </div>
+            <div className="testimonials-card-image-container">
+                <img src={testimonial.image} alt="Testimonial Image" className="testimonials-card-image" />
+            </div>
           </div>
         ))}
       </div>
