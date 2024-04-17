d3d4ea56974017effd71fed893cb71c7cb4eed40
thuang79 <thuang79@my.bcit.ca>
2024-04-17 14:22:55 -0700

    more styling for customer profile

--- src/pages/CustomerProfile.jsx
+++ src/pages/CustomerProfile.jsx
@@ -1,63 +1,61 @@
-import React, { useState } from 'react';
+import React from 'react';
 import kitty from '../assets/kitty.jpg';
 
 function CustomerProfile () {
     return (
-            <div className="customer-profile">
-                <div className="customer-profile-top-container">
-                    <h1 className="customer-profile-title">Customer Profile</h1>
-                    <div className="customer-profile-buttons">
-                        <a href="#editprofile" className="button">EDIT PROFILE</a> 
-                        <a href="#notifications" className="button">NOTIFICATIONS</a>
-                    </div>
+        <div className="customer-profile">
+            <div className="customer-profile-top-container">
+                <h1 className="customer-profile-title">Customer Profile</h1>
+                <div className="customer-profile-buttons">
+                    <a href="#editprofile" className="button">EDIT PROFILE</a> 
+                    <a href="#notifications" className="button">NOTIFICATIONS</a>
                 </div>
-                <div className="customer-profile-image-container">
-                    <img src={kitty} alt="Profile Picture" className="customer-profile-picture"/>
+            </div>
+            <div className="customer-profile-image-container">
+                <img src={kitty} alt="Profile Picture" className="customer-profile-picture"/>
+            </div>
+            <div className="customer-profile-details"> 
+                <h2 className="customer-profile-name">Tracy Huang</h2>
+                <p>I love to go on hikes, and eat yummy food!</p>
+                <p><i className="fa-solid fa-envelope"></i> tracy@home.com</p>
+                <p><i className="fa-solid fa-phone"></i> +123 456 7890</p>
+                <p><i className="fa-solid fa-location-dot"></i> Vancouver, Canada</p>
+            </div>
+            <div className="customer-profile-history">
+                <div className="customer-profile-history-header">
+                    <h2 className="customer-profile-history-title">Appointment History</h2>
+                    <a href="#seeall" className="customer-profile-see-all-button">See All</a>
                 </div>
-        <div className="wrapper">
-                <div className="customer-profile-details"> 
-                    <h2 className="customer-profile-name">Tracy Huang</h2>
-                    <p>I love to go on hikes, and eat yummy food!</p>
-                    <p><i className="fa-solid fa-envelope"></i> tracy@home.com</p>
-                    <p><i className="fa-solid fa-phone"></i> +123 456 7890</p>
-                    <p><i className="fa-solid fa-location-dot"></i> Vancouver, Canada</p>
+                <table className="customer-profile-table">
+                    <tr>
+                        <th>Date</th>
+                        <th>Service</th>
+                        <th>Provider</th>
+                    </tr>
+                    <tr>
+                        <td>15 April, 2024</td>
+                        <td>Haircut & Styling</td>
+                        <td>Sarah</td>
+                    </tr>
+                </table>
+            </div>
+            <div className="customer-profile-upcoming">
+                <div className="customer-profile-upcoming-header">
+                    <h2 className="customer-profile-upcoming-title">Upcoming Appointments</h2>
+                    <a href="#seeall" className="customer-profile-see-all-button">See All</a>
                 </div>
-                <div className="customer-profile-history">
-                            <div className="customer-profile-history-header">
-                                <h2 className="customer-profile-history-title">Appointment History</h2>
-                                <a href="#seeall" className="customer-profile-see-all-button">See All</a>
-                            </div>
-                            <div className="customer-profile-history-date">
-                                <h4 className="customer-profile-appointment-heading">Date</h4>
-                                <p className="customer-profile-description">15 April, 2024</p>
-                            </div>
-                            <div className="customer-profile-history-service">
-                                <h4 className="customer-profile-appointment-heading">Service</h4>
-                                <p className="customer-profile-description">Haircut & Styling</p>
-                            </div>
-                            <div className="customer-profile-history-styling">
-                                <h4 className="customer-profile-appointment-heading">Stylist</h4>
-                                <p className="customer-profile-description">Sarah</p>
-                            </div>
-                </div>
-                <div className="customer-profile-upcoming">
-                        <div className="customer-profile-upcoming-header">
-                            <h2 className="customer-profile-upcoming-title">Upcoming Appointments </h2>
-                            <a href="#seeall" className="customer-profile-see-all-button">See All</a>
-                        </div>
-                            <div className="customer-profile-upcoming-date">
-                                <h4 className="customer-profile-appointment-heading">Date</h4>
-                                <p className="customer-profile-description">29, June, 2024</p>
-                            </div>
-                            <div className="customer-profile-upcoming-service">
-                                <h4 className="customer-profile-appointment-heading">Service</h4>
-                                <p className="customer-profile-description">Gel X Nails</p>
-                            </div>
-                            <div className="customer-profile-upcoming-styling">
-                                <h4 className="customer-profile-appointment-heading">Stylist</h4>
-                                <p className="customer-profile-description">Joe</p>
-                            </div>
-                </div>
+                <table className="customer-profile-table">
+                    <tr>
+                        <th>Date</th>
+                        <th>Service</th>
+                        <th>Provider</th>
+                    </tr>
+                    <tr>
+                        <td>29 June, 2024</td>
+                        <td>Haircut & Styling</td>
+                        <td>Joe</td>
+                    </tr>
+                </table>
             </div>
         </div>
     )
