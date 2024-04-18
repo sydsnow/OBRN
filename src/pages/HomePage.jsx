c2ddfad8e679604771a06dccb307aff57df222e1
Pamela Pragides <ppragides1@my.bcit.ca>
2024-04-18 10:43:22 -0700

    fixed customer registration form

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 // import Carousel from "../components/Carousel";
 import HomeBanner from "../components/HomeBanner";
+import HomeServices from "../components/HomeServices";
 
 function HomePage () {
     return (
         <main className="main">
         {/* <Carousel /> */}
         <HomeBanner />
+        <HomeServices />
 
         </main>
     )
