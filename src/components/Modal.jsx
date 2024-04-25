0d7261d5b9330cec85c1c3d94fbe009436b3c958
Sydnee Snowball <s.snowball@hotmail.com>
2024-04-25 11:51:28 -0700

    testimonial modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 //import React from 'react';
 import Modal from 'react-modal';
 import PropTypes from 'prop-types';
-import "../scss/components/_modal.scss";
+//import "../scss/components/_modal.scss";
 
 function ModalComponent({ isOpen, onRequestClose, description, closeModal }) {
   // Define custom styles for the modal
