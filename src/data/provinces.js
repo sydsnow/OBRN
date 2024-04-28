19da0cfb2f591637874b58b332b65392e206c7bc
Carissa Ward <cward52@my.bcit.ca>
2024-04-28 12:23:25 -0700

    province dropdown added to business registration

---
+++ src/data/provinces.js
@@ -0,0 +1,18 @@
+const provinces = [
+    'Alberta',
+    'British Columbia',
+    'Manitoba',
+    'New Brunswick',
+    'Newfoundland and Labrador',
+    'Northwest Territories',
+    'Nova Scotia',
+    'Nunavut',
+    'Ontario',
+    'Prince Edward Island',
+    'Quebec',
+    'Saskatchewan',
+    'Yukon',
+  ];
+  
+  export default provinces;
+  
