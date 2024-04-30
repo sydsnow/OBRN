b867fba413c5c6b404b1cab1233d0f9eb151cebb
Sydnee Snowball <s.snowball@hotmail.com>
2024-04-30 13:06:11 -0700

    more input validation for security

--- src/pages/AdminEditCustomer.jsx
+++ src/pages/AdminEditCustomer.jsx
@@ -1,4 +1,191 @@
-import { Link,  useParams } from 'react-router-dom';
+// import { Link,  useParams } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+// function AdminEditCustomer() {
+//     const { id } = useParams(); // Fetch the customer ID from the URL params
+//     const [customer, setCustomer] = useState(null);
+//     const [editing, setEditing] = useState(false);
+
+//     useEffect(() => {
+//         // Fetch customer details when component mounts
+//         const fetchCustomer = async () => {
+//             try {
+//                 const apiUrl = import.meta.env.VITE_API_BASE_URL;
+//                 const response = await axios.get(`${apiUrl}/api/Customer/getcustomer/${id}`);
+//                 setCustomer(response.data); // Assuming response.data is the customer details
+//             } catch (error) {
+//                 console.error('Failed to fetch customer: ', error);
+//             }
+//         };
+
+//         fetchCustomer();
+
+//         // Cleanup function
+//         return () => {
+//             // Cleanup logic if needed
+//         };
+//     }, [id]);
+
+//     const handleInputChange = (e) => {
+//         setCustomer({ ...customer, [e.target.name]: e.target.value });
+//     };
+
+//     const handleIsVip = () => {
+//         setCustomer({ ...customer, vip: !customer.vip });
+//     };
+    
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         try {
+//             const apiUrl = import.meta.env.VITE_API_BASE_URL;
+//             const response = await axios.post(`${apiUrl}/api/Customer/editcustomer/`, customer);
+//             console.log('Customer updated:', response.data);
+//             setEditing(false);
+//         } catch (error) {
+//             console.error('Failed to update customer: ', error);
+//         }
+//     };
+
+//     if (!customer) {
+//         return <div>Loading...</div>;
+//     }
+
+//     return (
+//         <div className="wrapper">
+//             <div className="admin">
+//                 <Link to="/admin-all-customers"><button>Back to Customers</button></Link>
+//                 <div className="admin-edit-container">
+//                     <h1>Edit Customer Details</h1>
+
+//                     <div className="admin-customer-details">
+//                     <div className="admin-customer-info">
+//                             <label>VIP</label>
+//                             {editing ? (
+//                                 <input
+//                                     type="checkbox"
+//                                     name="vip"
+//                                     checked={customer.isVerified}
+//                                     onChange={handleIsVip}
+//                                 />
+//                             ) : (
+//                                 <p>{customer.vip ? 'Yes' : 'No'}</p>
+//                             )}
+//                          </div>    
+//                         <div className="admin-customer-info">
+//                             <label>First Name:</label>
+//                             {editing ? (
+//                                 <input
+//                                     type="text"
+//                                     name="firstName"
+//                                     value={customer.firstName}
+//                                     onChange={handleInputChange}
+//                                 />
+//                             ) : (
+//                                 <p>{customer.firstName}</p>
+//                             )}
+//                         </div>
+//                         <div className="admin-customer-info">
+//                             <label>Last Name:</label>
+//                             {editing ? (
+//                                 <input
+//                                     type="text"
+//                                     name="lastName"
+//                                     value={customer.lastName}
+//                                     onChange={handleInputChange}
+//                                 />
+//                             ) : (
+//                                 <p>{customer.lastName}</p>
+//                             )}
+//                         </div>
+//                         <div className="admin-customer-info">
+//                             <label>Email:</label>
+//                             {editing ? (
+//                                 <input
+//                                     type="text"
+//                                     name="email"
+//                                     value={customer.email}
+//                                     onChange={handleInputChange}
+//                                 />
+//                             ) : (
+//                                 <p>{customer.email}</p>
+//                             )}
+//                         </div>
+
+
+
+//                         <div className="admin-customer-info">
+//                             <label>Phone:</label>
+//                             {editing ? (
+//                                 <input
+//                                     type="text"
+//                                     name="phone"
+//                                     value={customer.phone}
+//                                     onChange={handleInputChange}
+//                                 />
+//                             ) : (
+//                                 <p>{customer.phone}</p>
+//                             )}
+//                             </div>
+//                             <div className="admin-customer-info">
+//                             <label>Username:</label>
+//                             {editing ? (
+//                                 <input
+//                                 className='readonly-input'
+//                                     type="text"
+//                                     name="username"
+//                                     readOnly
+//                                     value={customer.pkCustomerId}
+//                                     onChange={handleInputChange}
+//                                 />
+//                             ) : (
+//                                 <p>{customer.pkCustomerId}</p>
+//                             )}
+//                         </div>
+
+//                         <div className="admin-customer-info">
+//                             <label>Birthdate:</label>
+//                             {editing ? (
+//                                 <input
+//                                 className='readonly-input'
+//                                     type="text"
+//                                     name="birthdate"
+//                                     readOnly
+//                                     value={customer.birthdate}
+//                                     onChange={handleInputChange}
+//                                 />
+//                             ) : (
+//                                 <p>{customer.birthdate}</p>
+//                             )}
+//                         </div>
+                        
+//                         {/* <div className="admin-customer-info">
+//                             <label>VIP</label>
+//                             <p>{customer.vip ? 'Yes' : 'No'}</p>
+//                         </div> */}
+//                         <div className="admin-customer-info">
+//                             <label>Is Over the Age of 18?</label>
+//                             <p>{customer.confirm18 ? 'Yes' : 'No'}</p>
+//                         </div>
+
+//                     </div>
+
+//                     <div className="admin-edit-btns">
+//                         {editing ? (
+//                             <button onClick={handleSubmit}>Save</button>
+//                         ) : (
+//                             <button onClick={() => setEditing(true)}>Edit</button>
+//                         )}
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default AdminEditCustomer;
+import { Link, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -6,6 +193,7 @@ function AdminEditCustomer() {
     const { id } = useParams(); // Fetch the customer ID from the URL params
     const [customer, setCustomer] = useState(null);
     const [editing, setEditing] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
         // Fetch customer details when component mounts
@@ -34,10 +222,46 @@ function AdminEditCustomer() {
     const handleIsVip = () => {
         setCustomer({ ...customer, vip: !customer.vip });
     };
-    
+
+    const validateInput = () => {
+        if (!customer.firstName || !customer.lastName || !customer.email || !customer.phone) {
+            setErrorMessage('All fields are required.');
+            return false;
+        }
+
+        if (!validateEmail(customer.email)) {
+            setErrorMessage('Invalid email format.');
+            return false;
+        }
+
+        if (!validatePhone(customer.phone)) {
+            setErrorMessage('Invalid phone number format.');
+            return false;
+        }
+
+        // Additional validation logic for other fields can be added here
+
+        return true;
+    };
+
+    const validateEmail = (email) => {
+        // Regular expression for validating email format
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    const validatePhone = (phone) => {
+        // Regular expression for validating phone number format
+        const phoneRegex = /^\d{10}$/;
+        return phoneRegex.test(phone);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (!validateInput()) {
+            return;
+        }
+
         try {
             const apiUrl = import.meta.env.VITE_API_BASE_URL;
             const response = await axios.post(`${apiUrl}/api/Customer/editcustomer/`, customer);
@@ -58,9 +282,9 @@ function AdminEditCustomer() {
                 <Link to="/admin-all-customers"><button>Back to Customers</button></Link>
                 <div className="admin-edit-container">
                     <h1>Edit Customer Details</h1>
-
+                    {errorMessage && <p className="error-message">{errorMessage}</p>}
                     <div className="admin-customer-details">
-                    <div className="admin-customer-info">
+                        <div className="admin-customer-info">
                             <label>VIP</label>
                             {editing ? (
                                 <input
@@ -72,7 +296,7 @@ function AdminEditCustomer() {
                             ) : (
                                 <p>{customer.vip ? 'Yes' : 'No'}</p>
                             )}
-                         </div>    
+                        </div>
                         <div className="admin-customer-info">
                             <label>First Name:</label>
                             {editing ? (
@@ -103,7 +327,7 @@ function AdminEditCustomer() {
                             <label>Email:</label>
                             {editing ? (
                                 <input
-                                    type="text"
+                                    type="email"
                                     name="email"
                                     value={customer.email}
                                     onChange={handleInputChange}
@@ -112,14 +336,11 @@ function AdminEditCustomer() {
                                 <p>{customer.email}</p>
                             )}
                         </div>
-
-
-
                         <div className="admin-customer-info">
                             <label>Phone:</label>
                             {editing ? (
                                 <input
-                                    type="text"
+                                    type="tel"
                                     name="phone"
                                     value={customer.phone}
                                     onChange={handleInputChange}
@@ -127,12 +348,12 @@ function AdminEditCustomer() {
                             ) : (
                                 <p>{customer.phone}</p>
                             )}
-                            </div>
-                            <div className="admin-customer-info">
+                        </div>
+                        <div className="admin-customer-info">
                             <label>Username:</label>
                             {editing ? (
                                 <input
-                                className='readonly-input'
+                                    className='readonly-input'
                                     type="text"
                                     name="username"
                                     readOnly
@@ -143,15 +364,12 @@ function AdminEditCustomer() {
                                 <p>{customer.pkCustomerId}</p>
                             )}
                         </div>
-
                         <div className="admin-customer-info">
                             <label>Birthdate:</label>
                             {editing ? (
                                 <input
-                                className='readonly-input'
                                     type="text"
                                     name="birthdate"
-                                    readOnly
                                     value={customer.birthdate}
                                     onChange={handleInputChange}
                                 />
@@ -159,18 +377,11 @@ function AdminEditCustomer() {
                                 <p>{customer.birthdate}</p>
                             )}
                         </div>
-                        
-                        {/* <div className="admin-customer-info">
-                            <label>VIP</label>
-                            <p>{customer.vip ? 'Yes' : 'No'}</p>
-                        </div> */}
                         <div className="admin-customer-info">
                             <label>Is Over the Age of 18?</label>
                             <p>{customer.confirm18 ? 'Yes' : 'No'}</p>
                         </div>
-
                     </div>
-
                     <div className="admin-edit-btns">
                         {editing ? (
                             <button onClick={handleSubmit}>Save</button>
@@ -185,3 +396,4 @@ function AdminEditCustomer() {
 }
 
 export default AdminEditCustomer;
+
