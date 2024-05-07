8a87bc01e33be4c380c65b05db8ace9a9f2b75c3
Carissa Ward <cward52@my.bcit.ca>
2024-05-06 19:53:37 -0700

    removed logged api key

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -10,7 +10,6 @@ export const AuthProvider = ({ children }) => {
                     'Ocp-Apim-Subscription-Key': import.meta.env.VITE_API_KEY,
                 }
             });
-            console.log(import.meta.env.VITE_API_KEY);
             const { token } = response.data;
             localStorage.setItem('token', token.result);
             setAuthHeaders(token.result); 
