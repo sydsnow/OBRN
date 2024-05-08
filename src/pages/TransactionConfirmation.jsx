e0a2d09acc5e52cd902ef21352d1eb5a456d771f
thuang79 <thuang79@my.bcit.ca>
2024-05-08 11:00:28 -0700

    transaction confirmation page

---
+++ src/pages/TransactionConfirmation.jsx
@@ -0,0 +1,15 @@
+
+function TransactionConfirmation() {
+  return (
+    <div className="transaction-confirmation">
+      <div className="transaction-confirmation-text">
+        <h2>Woohoo! Your membership has been upgraded!</h2>
+        <p>
+          We're excited to let you know that your membership has been successfully upgraded. Enjoy your new benefits and thanks for being a valued member!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default TransactionConfirmation;
