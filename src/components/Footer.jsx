416779909cd9e26ba7e3307e776e1f3e715eafb8
thuang79 <thuang79@my.bcit.ca>
2024-05-07 21:16:15 -0700

    footer, top nav info, privacy statement
    put auth in business and customer registration, cleaned up the footer, updated nav info, and fixed error in privacy statement page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/obrn-logo.png';
 import { getYear } from "../utilities/utilities";
+import { useContext } from 'react';
+import { AuthContext } from '../auth/authUtils';
+import { getRolesFromJWT } from "../utilities/utilities";
 
 function Footer() {
+  const { isAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  let userRoles = [];
+
+  if (token) {
+    userRoles = getRolesFromJWT(token);
+  }
+
+  console.log('Customer and Business Registration is not shown in the footer:', isAuthenticated);
 
   return (
     <footer className="footer">
@@ -14,15 +27,24 @@ function Footer() {
           <h4 className="footer-heading">Quick Menu</h4>
           <Link to="/" className="footer-link">HOME</Link>
           <Link to="/services" className="footer-link">SERVICES</Link>
-          <Link to="/comingsoon" className="footer-link">SHOP</Link>
+          {!isAuthenticated && (
+            <>
               <Link to="/registerbusiness" className="footer-link">BUSINESS REGISTRATION</Link>
               <Link to="/registercustomer" className="footer-link">CUSTOMER REGISTRATION</Link>
+            </>
+          )}
+          {isAuthenticated && (
+            <Link to={userRoles.includes('customer') ? "/customerprofile" : "/businessprofile"} className="footer-link">
+              PROFILE
+            </Link>
+          )}
           <Link to="/admin" className="footer-link">ADMIN</Link>
         </div>
         <div className="footer-section">
-          <h4 className="footer-heading">Services</h4>
+          <h4 className="footer-heading">Categories</h4>
           <Link to="/comingsoon" className="footer-link">HAIR</Link>
           <Link to="/comingsoon" className="footer-link">NAILS</Link>
+          <Link to="/comingsoon" className="footer-link">FACIAL</Link>
           <Link to="/comingsoon" className="footer-link">SPA</Link>
         </div>
         <div className="footer-section">
@@ -31,14 +53,11 @@ function Footer() {
           <Link to="/termscondition" className="footer-link">TERMS & CONDITIONS</Link>
           <Link to="/privacystatement" className="footer-link">PRIVACY STATEMENT</Link>
           <Link to="/cancellationpolicy" className="footer-link">CANCELLATION POLICY</Link>
-          <Link to="/businessprofile" className="footer-link">BUSINESS PROFILE</Link>
-          <Link to="/customerprofile" className="footer-link">CUSTOMER PROFILE</Link>
         </div>
         <div className="footer-section">
           <h4 className="footer-heading">Contact Us</h4>
-          <p>4140 PARKER RD, ALLENTOWN, NEW MEXICO 31134</p>
           <p>OURBEAUTY@GMAIL.COM</p>
-          <p>(+92)3012699778</p>
+          <p>(604) 902-5234</p>
         </div>
       </div>
       <div className="footer-bottom">
--- src/pages/PrivacyStatement.jsx
+++ src/pages/PrivacyStatement.jsx
@@ -1,9 +1,9 @@
 function PrivacyStatement() {
     return (
         <div className="privacy-statement">
-                <div className="testimonials-banner">
-                    <p className="testimonials-small">PRIVACY STATEMENT</p>
-                    <p className="testimonials-large">Privacy Statement</p>
+            <div className="testimonials-banner">
+                <p className="testimonials-small">PRIVACY STATEMENT</p>
+                <p className="testimonials-large">Privacy Statement</p>
                 <div className="testimonials-path">
                     <i className="fa-solid fa-house"></i>
                     <p>HOME</p>
@@ -13,30 +13,40 @@ function PrivacyStatement() {
             </div>
             <div className="privacy-statement-container">
                 <div className="wrapper-info-content">
+                    <h3>Privacy Policy</h3>
                     <p>
-                    <h3>Privacy Policy<br/></h3> 
-
-When you purchase anything from our website, as part of the buying and selling process, we collect the personal information you give us such as your name, address and email address. When you browse our website, we also automatically receive your computer’s internet protocol (IP) address in order to provide us with information that helps us learn about your browser and operating system.
- 
-<br/><br/><h3>Email marketing</h3>
-With your permission, we may send you emails, when applicable about our company, new products and other updates and you may request to unsubscribe at any time.
- 
-<br/><br/><h3>Consent</h3> 
-How do we receive your consent?
-When you provide us with personal information to complete a transaction, verify your credit card, place an order, arrange for a delivery or return a purchase, we infer that you consent to our collecting it and using it for that specific reason only.
-If we ask for your personal information for a secondary reason, like marketing, we will either ask you directly for your expressed consent, or provide you with an opportunity to say no.
-
-<br/><br/><h3>Security</h3>
-To protect your personal information, we take reasonable precautions and follow industry best practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed.
-If you provide us with your credit card information, the information is encrypted using secure socket layer technology (SSL) and stored with 2FA  encryption with our trusted merchant providers. Our Beauty Referral Network does not keep or Store your credit card information..
-Although no method of transmission over the Internet or electronic storage is 100% secure, we follow all PCI-DSS requirements and implement additional generally accepted industry standards.
-
-<br/><br/><h3>Age</h3>
-We require all members of our organization to be above the legal age of 18 years old. 
-
-
-
-
+                        When you purchase anything from our website, as part of the buying and selling process,
+                        we collect the personal information you give us such as your name, address and email address.
+                        When you browse our website, we also automatically receive your computer’s internet protocol
+                        (IP) address in order to provide us with information that helps us learn about your browser
+                        and operating system.
+                    </p>
+                    <h3>Email marketing</h3>
+                    <p>
+                        With your permission, we may send you emails, when applicable about our company, new products
+                        and other updates and you may request to unsubscribe at any time.
+                    </p>
+                    <h3>Consent</h3>
+                    <p>
+                        How do we receive your consent? When you provide us with personal information to complete a
+                        transaction, verify your credit card, place an order, arrange for a delivery or return a purchase,
+                        we infer that you consent to our collecting it and using it for that specific reason only.
+                        If we ask for your personal information for a secondary reason, like marketing, we will either
+                        ask you directly for your expressed consent, or provide you with an opportunity to say no.
+                    </p>
+                    <h3>Security</h3>
+                    <p>
+                        To protect your personal information, we take reasonable precautions and follow industry best
+                        practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed.
+                        If you provide us with your credit card information, the information is encrypted using secure
+                        socket layer technology (SSL) and stored with 2FA encryption with our trusted merchant providers.
+                        Our Beauty Referral Network does not keep or Store your credit card information.
+                        Although no method of transmission over the Internet or electronic storage is 100% secure, we
+                        follow all PCI-DSS requirements and implement additional generally accepted industry standards.
+                    </p>
+                    <h3>Age</h3>
+                    <p>
+                        We require all members of our organization to be above the legal age of 18 years old.
                     </p>
                 </div>
             </div>
