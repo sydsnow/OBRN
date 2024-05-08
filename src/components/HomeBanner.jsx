4cc72ecb18d1b0120043d0a2326e512d5e41ca00
thuang79 <thuang79@my.bcit.ca>
2024-05-07 21:43:41 -0700

    buttons in home banner
    fix the buttons in the home banner so it will automatically re-appear when a user logs out

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,59 +1,60 @@
-import { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { getEmailFromJWT, getRolesFromJWT } from '../utilities/utilities';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../auth/authUtils'; 
 
 function HomeBanner() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const token = localStorage.getItem('token');
-    const apiUrl = import.meta.env.VITE_API_BASE_URL;
+    const { isAuthenticated } = useAuth(); 
 
     useEffect(() => {
+        console.log('Join buttons is not shown in home banner:', isAuthenticated); 
+
+        const token = localStorage.getItem('token');
+        const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
         async function confirmUser() {
-            if (token) {
+            if (token && !isAuthenticated) {
                 const roles = getRolesFromJWT(token);
                 const email = getEmailFromJWT(token);
+                let response;
                 if (roles.includes('customer')) {
-                    const response = await axios.get(`${apiUrl}/api/customer/get-customer-by-email?email=${email}`);
-                    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-                    if (response.data) {
-                        setIsAuthenticated(true);
-                    }
+                    response = await axios.get(`${apiUrl}/api/customer/get-customer-by-email?email=${email}`);
                 } else {
-                    const response = await axios.get(`${apiUrl}/api/business/get-business-by-email?email=${email}`);
-                    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-                    if (response.data) {
-                        setIsAuthenticated(true);
-                    }
+                    response = await axios.get(`${apiUrl}/api/business/get-business-by-email?email=${email}`);
+                }
+                axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+                if (response.data) {
                 }
             }
         }
         confirmUser();
-    }, [token, apiUrl]);
+    }, [isAuthenticated]); 
 
     return (
         <div className="home">
-        <div className="home-banner">
-            <div className="home-banner-container">
-            <div className="home-banner-copy">
-            <h1>The World&apos;s Largest Online Referral Network</h1>
-            <p>World&apos;s No.1 Referral Portal</p>
+            <div className="home-banner">
+                <div className="home-banner-container">
+                    <div className="home-banner-copy">
+                        <h1>The World&apos;s Largest Online Referral Network</h1>
+                        <p>World&apos;s No.1 Referral Portal</p>
+                    </div>
+                    <div className="home-btns">
+                        {!isAuthenticated && (
+                            <>
+                                <Link to="/registerbusiness">
+                                    <button className="home-btn">Join as a Business</button>
+                                </Link>
+                                <Link to="/registercustomer">
+                                    <button className="home-btn">Join as a Customer</button>
+                                </Link>
+                            </>
+                        )}
+                    </div>
+                </div>
             </div>
-            <div className="home-btns">
-            {(!isAuthenticated && !token) &&
-                <>
-                    <Link to="/registerbusiness">
-                            <button className="home-btn">Join as a Business</button>
-                    </Link>
-                    <Link to="/registercustomer">
-                            <button className="home-btn">Join as a Customer</button>
-                    </Link>
-                </>
-            }
-            </div>
-            </div>
-        </div>
         </div>
     );
 }
+
 export default HomeBanner;
